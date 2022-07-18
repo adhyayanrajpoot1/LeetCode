8059dc9070e87dc758aa81501c31d4c47bd0df9d
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-07-18 22:29:57 +0530

    Time: 18 ms (36.50%), Space: 14.3 MB (26.16%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int original;
+    void flood_fill(int r , int c , vector<vector<int>>& image,int color)
+    {
+        int n = image.size();
+        int m = image[0].size();
+        image[r][c] = color;
+        int d[] = {0,1,0,-1,0};
+        
+        for(int k = 0;  k < 4 ; k++)
+        {
+            int dx = r+d[k];
+            int dy = c+d[k+1];
+            if(dx>=0 && dy>=0 && dx<n && dy<m && image[dx][dy]==original)
+            {
+                flood_fill(dx,dy,image,color);
+            }
+        }
+        
+    }
+    vector<vector<int>>vis;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+        vis.resize(n,vector<int>(m,0));
+        this->original = image[sr][sc];
+        if(original==color)return image;
+        flood_fill(sr,sc,image,color);
+        
+        
+//         for(int i = sr ; i < n ; i++)
+//         {
+//             for(int j = sc ; j < m ; j++)
+//             {
+//                 if()
+//             }
+//         }
+        return image;
+    }
+};
