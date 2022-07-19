7f6d7b0595c83015019df77a86b516958a9c49c1
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-07-19 19:07:42 +0530

    Time: 6 ms (89.01%), Space: 11.3 MB (56.53%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int>v;
+    void f(int ind , int target , vector<int>&can,vector<vector<int>>&ans)
+    {
+        
+        if(ind==can.size())
+        {
+            if(target==0){
+                ans.push_back(v);
+            }
+            return;
+        }
+        if(can[ind]<=target)
+        {
+            v.push_back(can[ind]);
+            f(ind,target-can[ind],can,ans);
+            v.pop_back();
+        }
+        f(ind+1,target,can,ans);
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& c, int target) {
+        vector<vector<int>>ans;
+        f(0,target,c,ans);
+        return ans;
+    }
+};
