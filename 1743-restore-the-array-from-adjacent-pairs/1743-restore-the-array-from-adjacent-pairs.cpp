3b04eeab6b235e6c673566f8621d927e22ecd8e4
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-07-21 19:16:13 +0530

    Time: 1598 ms (5.12%), Space: 161.5 MB (27.28%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    unordered_map<int,vector<int>>mapp;
+    set<int>v;
+    vector<int>ans;
+    void dfs(int ele)
+    {
+        if(v.find(ele)!=v.end())return;
+        ans.push_back(ele);
+        v.insert(ele);
+        for(auto it : mapp[ele])
+        {
+           dfs(it);
+        }
+    }
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        int n = adjacentPairs.size();
+        
+        for(auto it: adjacentPairs)
+        {
+            mapp[it[0]].push_back(it[1]);
+            mapp[it[1]].push_back(it[0]);
+        }
+        int head;
+        
+        for(auto it : mapp)
+        {
+            if(it.second.size()==1){
+                head=it.first;
+                break;
+            }
+        }
+        dfs(head);
+        return ans;
+        
+    }
+};
