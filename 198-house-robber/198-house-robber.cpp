36d00598e48893c1e28d5bb677d3a1d89aee044e
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-07-25 09:56:45 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (94.89%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -13,6 +13,27 @@ public:
     int rob(vector<int>& nums) {
         //if(nums.size()==1 || nums.size()==2)return nums[nums.size()-1];
         memset(memo,-1,sizeof(memo));
-        return f(nums.size()-1,nums);
+        int n = nums.size();
+        if(n==0)return 0;
+        if(n==1)return nums[0];
+        int dp[n+1];
+        memset(dp,0,sizeof(dp));
+        dp[0]=nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i =2 ; i< n ; i++)
+        {
+            dp[i] = max(dp[i-2]+nums[i] , dp[i-1]);
+        }
+        return dp[n-1];
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //return f(nums.size()-1,nums);
     }
 };
