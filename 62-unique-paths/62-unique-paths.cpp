70d925332660056206973f28bef1ea3803355b79
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-07-28 09:56:42 +0530

    Time: 2 ms (49.32%), Space: 6 MB (61.31%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int ans = 0;
+    int dp[101][101];
+    int f(int m , int n)
+    {
+        if(m==1 && n==1)return 1;
+        if(dp[m][n]!=-1)return dp[m][n];
+        int small = 0;
+        if(m>1)
+        small +=f(m-1,n);
+        if(n>1)small+=f(m,n-1);
+        return dp[m][n] = small;
+        
+    }
+    int uniquePaths(int m, int n) {
+        memset(dp,-1,sizeof dp);
+        return f(m,n);
+        return ans;
+    }
+};
