e82b00ed40263607cd9eb9b7cc9951e0e524a84e
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-07-28 15:59:18 +0530

    Time: 46 ms (69.00%), Space: 12.3 MB (83.64%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>>visited;
-
-void flood_fill(int i , int j ,vector<vector<char>>& grid)
-{
-    int n = grid.size();
-    int m = grid[0].size();
-    visited[i][j]=1;
-    int dx[] = {0,1,0,-1,0};
-    for(int k = 0 ; k < 4 ; k++)
+    
+    void f(int i , int j,vector<vector<char>>& grid)
     {
-        int xx = i+dx[k];
-        int yy = j+dx[k+1];
-        if(xx>=0&&yy>=0 && xx<n && yy<m && visited[xx][yy]==0 && grid[xx][yy]=='1')
+        grid[i][j] = '2';
+        int d[] = {0,-1,0,1,0};
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int k = 0 ; k < 4 ; k++)
         {
-            flood_fill(xx,yy,grid);
-        }
-    }
-}
-int numIslands( vector<vector<char>> grid)
-{
-    int n = grid.size();
-    int m = grid[0].size();
-    visited.resize(n , vector<int>(m,0));
-    int count=0;
-    for(int i = 0 ; i < n ; i++)
-    {
-        for(int j = 0 ; j < m ;j++)
-        {
-            if(grid[i][j]=='1' and visited[i][j]==0)
+            //cout<<grid[i][j]<<endl;
+            int xx = d[k]+i;
+            int yy = d[k+1]+j;
+            if(xx<n && xx>=0 && yy>=0 && yy<m && grid[xx][yy]=='1')
             {
-                count++;
-                flood_fill(i,j,grid);
+                //cout<<grid[i][j]<<endl;
+                f(xx,yy,grid);
+                
             }
         }
     }
-    return count;
-}
+    
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            for(int j = 0 ; j < m ; j++)
+            {
+                //cout<<grid[i][j]<<endl;
+                if(grid[i][j]=='1'){
+                    count++;
+                    f(i,j,grid);
+                    //cout<<grid[i][j]<<endl;
+                }
+            }
+        }
+        return count;
+        
+        
+    }
 };
