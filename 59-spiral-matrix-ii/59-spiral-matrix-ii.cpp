c660c8b0005f41f57369f105d990c9f19cc4c7ea
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-07-29 19:58:06 +0530

    Time: 3 ms (42.06%), Space: 6.6 MB (64.85%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,57 +1,31 @@
 class Solution {
 public:
+vector<vector<int>> generateMatrix(int n) {
 
-    vector<vector<int>> generateMatrix(int n) {
-
-        vector<vector<int>> result (n, vector<int>(n));
-        int cnt = 1;
-        for (int layer = 0; layer < (n + 1) / 2; layer++) {
-            // direction 1 - traverse from left to right
-            for (int ptr = layer; ptr < n - layer; ptr++) {
-                result[layer][ptr] = cnt++;
-            }
-            // direction 2 - traverse from top to bottom
-            for (int ptr = layer + 1; ptr < n - layer; ptr++) {
-                result[ptr][n - layer - 1] = cnt++;
-            }
-            // direction 3 - traverse from right to left
-            for (int ptr = n - layer - 2; ptr >= layer; ptr--) {
-                result[n - layer - 1][ptr] = cnt++;
-            }
-            // direction 4 - traverse from bottom to top
-            for (int ptr = n - layer - 2; ptr > layer; ptr--) {
-                result[ptr][layer] = cnt++;
-            }
-        }
-
-        return result;
+    int r1=0,r2=n-1;
+    int c1=0,c2=n-1;
+    int val=0;
+    
+    vector<vector<int>>v(n,vector<int>(n));
+    
+    while(r1<=r2 && c1<=c2)
+    {
+        for(int i=c1;i<=c2;i++)//left to right move,row is fixed
+            v[r1][i] = ++val;
+       
+        for(int i=r1+1;i<=r2;i++) //top to bootom move ,colomn is fixed
+            v[i][c2] = ++val;
+       
+        for(int i=c2-1;i>=c1;i--) // right to left move,row is fixed
+            v[r2][i] = ++val;
+        
+        for(int i=r2-1;i>r1;i--) // bottom to up move ,colomn is fixed
+            v[i][c1] = ++val;
+        r1++;
+        c1++;
+        r2--;
+        c2--;
     }
+    return v;
+}
 };
-
-
-// class Solution {
-// public:
-
-//     int floorMod(int x, int y) {
-//         return ((x % y) + y) % y;
-//     }
-
-//     vector<vector<int>> generateMatrix(int n) {
-//         vector<vector<int>> result (n, vector<int>(n));
-//         int cnt = 1;
-//         int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-//        int d = 0;
-//         int row = 0;
-//         int col = 0;
-//         while (cnt <= n * n) {
-//             result[row][col] = cnt++;
-//             int r = floorMod(row + dir[d][0], n);
-//             int c = floorMod(col + dir[d][1], n);
-//             // change direction if next cell is non zero
-//             if (result[r][c] != 0) d = (d + 1) % 4;
-//             row += dir[d][0];
-//             col += dir[d][1];
-//         }
-//         return result;
-//     }
-// };
