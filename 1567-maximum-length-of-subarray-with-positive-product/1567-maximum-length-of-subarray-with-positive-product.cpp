cb4119e3a904ab0d27037ed2a9c9488d6cd19bea
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-07-30 10:53:39 +0530

    Time: 153 ms (66.57%), Space: 57.9 MB (77.63%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
@@ -2,32 +2,66 @@ class Solution {
 public:
     int getMaxLen(vector<int>& nums) {
         int n = nums.size();
-        int countN = 0;
-        int countP = 0,temp;
-        int maxi = INT_MIN;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i]==0)
+        int ans = 0;
+        for(int i = 0; i < n ;)
+        {
+            int s = i;
+            while(s<n and nums[s]==0)s++;
+            int e = s;
+            int sn = -1, en = -1;
+            int countN =0 ;
+            while(e<n and nums[e]!=0)
             {
-                countN = 0;
-                countP = 0;
-                    
-            }
-            else if(nums[i] > 0){
-                countP++;
-                if(countN>0)
+                if(nums[e]<0){
                     countN++;
+                    if(sn==-1)sn=e;
+                    en = e;
+                }
+                e++;
             }
+            if(countN%2==0)ans = max(ans,e-s);
             else{
-                if(countN>0)
-                    temp = countN + 1;
-                else
-                    temp = 0;
-                countN = countP+1;
-                countP = temp;
+                if(sn!=-1)ans=max(ans,e-sn-1);
+                if(en!=-1)ans=max(ans,en-s);
             }
-            maxi = max(maxi, countP);
+            i=e+1;
         }
+        return ans;
         
-        return maxi;
+        
     }
 };
+
+// class Solution {
+// public:
+//     int getMaxLen(vector<int>& nums) {
+//         int n = nums.size();
+//         int countN = 0;
+//         int countP = 0,temp;
+//         int maxi = INT_MIN;
+//         for(int i = 0 ; i < n ; i++){
+//             if(nums[i]==0)
+//             {
+//                 countN = 0;
+//                 countP = 0;
+                    
+//             }
+//             else if(nums[i] > 0){
+//                 countP++;
+//                 if(countN>0)
+//                     countN++;
+//             }
+//             else{
+//                 if(countN>0)
+//                     temp = countN + 1;
+//                 else
+//                     temp = 0;
+//                 countN = countP+1;
+//                 countP = temp;
+//             }
+//             maxi = max(maxi, countP);
+//         }
+        
+//         return maxi;
+//     }
+// };
