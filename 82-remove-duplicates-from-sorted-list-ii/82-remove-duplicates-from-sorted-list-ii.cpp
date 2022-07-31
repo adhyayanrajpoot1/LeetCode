25b5db2cfa0dd6423e85e6422efcf04081d60d98
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-07-31 22:24:15 +0530

    Time: 14 ms (47.75%), Space: 11.2 MB (58.67%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -15,10 +15,12 @@ public:
         ListNode *prev=dummy;
         while(head!=NULL){
         if(head->next!=NULL && head->val==head->next->val){
-            while(head->next!=NULL && head->val==head->next->val)head=head->next;
-                prev->next=head->next;
-            }
-        else prev=prev->next;
+            while(head->next!=NULL && head->val==head->next->val)
+                head=head->next;
+            prev->next=head->next; 
+        }
+            
+        else prev=head;
         head=head->next;
         }
         return dummy->next;
