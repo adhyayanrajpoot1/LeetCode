d81d81189919e1db2d0a0f5492e62b74385791a3
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-08-03 10:53:16 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (55.13%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool wordPattern(string p, string s) {
+        stringstream check1(s);
+        string intermediate;
+        vector<string>v;
+        unordered_map<char,string>mp;
+        unordered_map<string,char>mp1;
+        int j =0;
+         set<char>s1;
+        set<string>s2;
+        while(getline(check1 , intermediate , ' '))
+        {
+            s2.insert(intermediate);
+            v.push_back(intermediate);
+        }
+       for(auto it : p)s1.insert(it);
+        for(int i=0; i < v.size() ; i++)
+        {
+            if(mp.find(p[j])!=mp.end() || mp1.find(v[i])!=mp1.end())
+            {
+                if(mp[p[j]]==v[i] && mp1[v[i]]==p[j++])continue;
+                else return false;
+            }
+            mp1[v[i]] = p[j];
+            mp[p[j++]] = v[i];
+            
+        }
+        if(mp.size()<s1.size() || mp1.size()<s2.size())return false;
+        return true;
+        
+    }
+};
