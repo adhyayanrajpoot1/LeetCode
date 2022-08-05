f0fa1a52756641c7e5b349acc8ddce3e5712fd48
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-08-05 19:44:46 +0530

    Time: 3 ms (62.90%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int dp[10000];
+    uint dp[10000];
     int f(int tar,vector<int>&nums) {
        if(tar==0)return 1;
-        if(tar<= 0 )return 0;
+        if(tar<0 )return 0;
         int ans = 0;
         if(dp[tar]!=-1)return dp[tar];
         for(auto it : nums)
@@ -17,8 +17,19 @@ public:
     
     int combinationSum4(vector<int>& nums, int target) {
         int n = nums.size();
-        memset(dp,-1,sizeof dp);
-        return f(target,nums);
+        memset(dp,0,sizeof dp);
+        //return f(target,nums);
         //return ans.size();
+        //int ans = 0;
+        dp[0] = 1;
+        for(int tar = 1; tar <=target ; tar++)
+        {
+            for(auto it : nums)
+            {
+                if(tar-it>=0)
+                    dp[tar]+=dp[tar-it];
+            }
+        }
+        return dp[target];
     }
 };
