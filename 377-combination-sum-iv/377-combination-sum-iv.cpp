064e561803a45ec367d2e7bf7bf000e1a3b27978
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-08-05 18:57:15 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-int combinationSum4(vector<int>& nums, int target) {
-	vector<uint> dp(target + 1); dp[0] = 1;
-	// start filling the dp array from bottom, i.e curTarget = 1 till target.
-	for (int curTarget = 1; curTarget <= target; curTarget++) 
-        for (auto& num : nums)     // check for all elements from nums
-            if (num <= curTarget)  // an element will only be taken if it is less than target to be achieved
-                // the optimal answer(no. of combination) of remaining value is added to dp[curTarget]
-				dp[curTarget] += dp[curTarget - num];   
-    return dp[target];
-}
+    int dp[10000];
+    int f(int tar,vector<int>&nums) {
+       if(tar==0)return 1;
+        if(tar<= 0 )return 0;
+        int ans = 0;
+        if(dp[tar]!=-1)return dp[tar];
+        for(auto it : nums)
+        {
+            if(tar-it>=0)
+                ans+=f(tar-it,nums);
+        }
+        return dp[tar] = ans;
+        
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        memset(dp,-1,sizeof dp);
+        return f(target,nums);
+        //return ans.size();
+    }
 };
