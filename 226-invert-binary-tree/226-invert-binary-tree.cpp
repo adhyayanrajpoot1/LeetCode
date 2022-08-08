8c8e7aca717a00bb808d93e314f3ef248df1b1ae
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-08-08 17:54:31 +0530

    Time: 3 ms (70.96%), Space: 9.6 MB (97.32%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -9,13 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
+        stack<TreeNode*> s;
+        s.push(root);
         if(root==NULL)return NULL;
-        swap(root->left , root->right);
-        invertTree(root->left);
-        invertTree(root->right);
+        while (!s.empty()) {
+            TreeNode* curr = s.top();
+            s.pop();
+            if(curr!=NULL && curr->left)
+                s.push(curr->left);
+            if(curr &&curr->right)
+                s.push(curr->right);
+            swap(curr->left, curr->right);
+        }
         return root;
     }
 };
+
+// class Solution {
+// public:
+//     TreeNode* invertTree(TreeNode* root) {
+//         if(root==NULL)return NULL;
+//         swap(root->left , root->right);
+//         invertTree(root->left);
+//         invertTree(root->right);
+//         return root;
+//     }
+// };
