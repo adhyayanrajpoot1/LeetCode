970372a3bf3ae7474d8e81835938ad4ca74eb316
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-08-08 08:43:56 +0530

    Time: 305 ms (14.64%), Space: 90.9 MB (9.34%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,19 +1,57 @@
+#include<unordered_map>
+class Solution {
+    unordered_map<int,vector<int>> m;
+    int MOD = 1e9+7;
+    
 
-    class Solution {
-public:
-    int countVowelPermutation(int n) {
-        const int MOD = 1e9 + 7;
-        long dp[5][n+1], ans = 0;
-        dp[0][1] = dp[1][1] = dp[2][1] = dp[3][1] = dp[4][1] = 1;
-        for(int i = 2; i <= n; i++) {
-            dp[0][i] =  dp[1][i-1];
-            dp[1][i] = (dp[0][i-1] + dp[2][i-1]) % MOD;
-            dp[2][i] = (dp[0][i-1] + dp[1][i-1] + dp[3][i-1] + dp[4][i-1]) % MOD;
-            dp[3][i] = (dp[2][i-1] + dp[4][i-1]) % MOD;
-            dp[4][i] =  dp[0][i-1];
+    public:
+
+        int helper(int n, int i, char prev, vector<vector<long long> > &dp){
+
+            if (i > n){
+                return 0;
+            }
+            if(i == n){
+                switch(prev)
+                {
+                    case 'a':
+                        return 1;
+                    case 'e':
+                        return 2;
+                    case 'i':
+                        return 4;
+                    case 'o':
+                        return 2;
+                    case 'u':
+                        return 1;
+                    default:
+                        return 5;
+                }
+            }
+
+            int idx = prev - 'a';
+            if(dp[i][idx] != -1){
+                return dp[i][idx];
+            }
+            long long ans = 0;
+            for(auto next : m[prev]){
+                ans += (helper(n, i+1, next, dp) % MOD);
+            }
+
+
+            return dp[i][idx] = ans % MOD;
         }
-        for(int i=0; i < 5; i++) 
-            ans = (ans + dp[i][n]) % MOD;
-        return ans;
-    }
+
+        int countVowelPermutation(int n) {
+            m['c'] = {'a','e','i','o','u'};
+            m['a'] = {'e'};
+            m['e'] = {'a','i'};
+            m['i'] = {'a','e','o','u'};
+            m['o'] = {'i','u'};
+            m['u'] = {'a'};
+
+            vector<vector<long long> > dp(vector<vector<long long> >(n+2,vector<long long>(27,-1)));
+            return helper(n , 1 , 'c', dp);
+
+        }
 };
