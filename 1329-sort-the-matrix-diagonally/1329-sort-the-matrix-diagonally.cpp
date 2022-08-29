f53b921fdd03547357c996ef27b3b848d2ec068c
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-08-29 19:04:14 +0530

    Time: 13 ms (76.99%), Space: 9.4 MB (52.62%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> mp;
-vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-    int m=mat.size();
-    int n=mat[0].size();
-    
-    for(int i=0;i<m;i++) {
-        for(int j=0;j<n;j++) {
-            mp[i-j].push_back(mat[i][j]);
+    void f(vector<vector<int>>& mat , int r , int c)
+    {
+        int n = mat.size();
+        int m = mat[0].size();
+        int*arr = new int[101]();
+        int i = r;
+        int j = c;
+        while(i<n && j <m)
+        {
+            arr[mat[i][j]]++;
+            i++;
+            j++;
+        }
+        i = r , j = c;
+        for(int k = 1 ; k < 101 ; k++)
+        {
+            //cout<<arr[k]<<" ";
+            while(arr[k]>0)
+            {
+                
+                mat[i][j] = k;
+                arr[k]--;
+                i++;
+                j++;
+            }
         }
     }
-    
-    for(int k=-(n-1);k<m;k++) {
-        sort(mp[k].begin(),mp[k].end());
-    }
-    
-    for(int i=m-1;i>=0;i--) {
-        for(int j=n-1;j>=0;j--) {
-            mat[i][j]=mp[i-j].back();
-            mp[i-j].pop_back();
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        for(int i = 0 ; i < m ; i++)
+        {
+            f(mat,0,i);
         }
+        for(int j = 1 ; j < n ; j++)
+        {
+            f(mat,j,0);
+        }
+        return mat;
     }
-    return mat;
-}
 };
