6452fd9e6f34f788ee7133ee6bbb3714f8ebfb0b
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-09-06 10:15:23 +0530

    Time: 58 ms (5.48%), Space: 11.6 MB (86.71%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -18,23 +18,32 @@ public:
 };
 */
 
-class Solution { // 20 ms, faster than 86.78%
+class Solution {
 public:
     vector<vector<int>> levelOrder(Node* root) {
-        if (root == nullptr) return {};
-        queue<Node*> q;
-        q.push(root);
-        vector<vector<int>> ans;
-        while (!q.empty()) {
-            ans.emplace_back();
-            for (int i = q.size(); i >= 1; i--) {
-                Node* curr = q.front(); q.pop();
-                ans.back().push_back(curr->val);
-                for (Node* child : curr->children) {
-                    q.push(child);
-                }
+    
+    vector<vector<int>> ans;
+    if(!root) return ans;
+    
+    queue<Node*>q;
+    q.push(root);
+    
+    while(!q.empty()){
+        
+        int sz=q.size();
+        vector<int> v;
+        
+        while(sz--){
+            auto curr=q.front();
+            q.pop();       
+            v.push_back(curr->val);
+  
+            for(auto it:curr->children){    //pushing curr node's children in queue
+                q.push(it);
             }
         }
-        return ans;
+        ans.push_back(v);
     }
+    return ans;
+}
 };
