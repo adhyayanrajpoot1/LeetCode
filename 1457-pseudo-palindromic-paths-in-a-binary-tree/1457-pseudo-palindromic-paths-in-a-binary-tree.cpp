d453279e40ddd5c4fdcec7cad10093adb805c14f
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-09-14 08:13:26 +0530

    Time: 824 ms (23.28%), Space: 192.2 MB (33.47%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,89 +1,52 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void rec(TreeNode* root) {
-        if (!root)
-            return;
-        
-        m[root->val]++;
-        
-        // If we got to a leaf - check if the path can be a polindrome
-        if (!root->left && !root->right) {
+    //   int pseudoPalindromicPaths(TreeNode* root, int count = 0) {
+    //     if (!root) return 0;
+    //     count ^= 1 << (root->val - 1);
+    //     int res = pseudoPalindromicPaths(root->left, count) + pseudoPalindromicPaths(root->right, count);
+    //     if (root->left == root->right && (count & (count - 1)) == 0) res++;
+    //     return res;
+    // }
+    int count=0;
+    void dfs(TreeNode*root,unordered_map<int,int>&v)
+    {
+        if(root==NULL)return;
+        v[root->val]++;
+        if(root->left==NULL && root->right==NULL)
+        {
             int odd = 0;
-            for (auto a : m)
-                if (a.second % 2 == 1)
-                    odd++;
-
-            if (odd <= 1)
-                res++;
-        }
-        
-        rec(root->left);
-        rec(root->right);
-        m[root->val]--;
-    }
-    
-    int pseudoPalindromicPaths (TreeNode* root) {
-        rec(root);
-        return res;
-    }
-    
-private:
-    int res = 0;
-    unordered_map<int, int> m;
-};
-
-
-
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int val;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//  * };
-//  */
-// class Solution {
-// public:
-//     //   int pseudoPalindromicPaths(TreeNode* root, int count = 0) {
-//     //     if (!root) return 0;
-//     //     count ^= 1 << (root->val - 1);
-//     //     int res = pseudoPalindromicPaths(root->left, count) + pseudoPalindromicPaths(root->right, count);
-//     //     if (root->left == root->right && (count & (count - 1)) == 0) res++;
-//     //     return res;
-//     // }
-//     int count=0;
-//     void dfs(TreeNode*root,unordered_map<int,int>v)
-//     {
-//         if(root==NULL)return;
-//         v[root->val]++;
-//         if(root->left==NULL && root->right==NULL)
-//         {
-//             int odd = 0;
-//             for(auto it : v)
-//             {
+            for(auto it : v)
+            {
                 
-//                 if(it.second%2==1){
-//                     odd++;
-//                 }
-//             }
-//             if(odd<=1)count++;
-//             return;
-//         }
-//         dfs(root->left,v);
-//         dfs(root->right,v);
+                if(it.second%2==1){
+                    odd++;
+                }
+            }
+            if(odd<=1)count++;
+            
+        }
+        dfs(root->left,v);
+        dfs(root->right,v);
         
-//         v[root->val]--;
+        v[root->val]--;
         
         
-//     }
-//     int pseudoPalindromicPaths (TreeNode* root) {
-//         //vector<int>v(10,0);
-//         unordered_map<int,int>v;
-//         dfs(root,v);
-//         return count;
-//     }
-// };
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        //vector<int>v(10,0);
+        unordered_map<int,int>v;
+        dfs(root,v);
+        return count;
+    }
+};
