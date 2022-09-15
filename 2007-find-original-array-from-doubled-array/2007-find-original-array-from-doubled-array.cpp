775e1a633c09045b729beb39575141badc38614a
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-09-15 21:47:57 +0530

    Time: 748 ms (43.79%), Space: 146.5 MB (34.22%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    vector<int> findOriginalArray(vector<int>& changed) {
-        map<int,int>m;
-        vector<int>ans;
-        int n = changed.size();
-        if(n%2)return {};
-        for(auto it : changed)m[it]++;
-        sort(changed.begin(),changed.end());
-        for(auto it : changed)
-        {
-            if(m[it]==0)continue;
-            if(m[it*2]==0)return {};
-            ans.push_back(it);
-            m[it]--;
-            m[it*2]--;
-        }
-        return ans;
+//     vector<int> findOriginalArray(vector<int>& changed) {
+//         map<int,int>m;
+//         vector<int>ans;
+//         int n = changed.size();
+//         if(n%2)return {};
+//         for(auto it : changed)m[it]++;
+//         sort(changed.begin(),changed.end());
+//         for(auto it : changed)
+//         {
+//             if(m[it]==0)continue;
+//             if(m[it*2]==0)return {};
+//             ans.push_back(it);
+//             m[it]--;
+//             m[it*2]--;
+//         }
+//         return ans;
         
+//     }
+     vector<int> findOriginalArray(vector<int>& A) {
+        if (A.size() % 2) return {};
+        unordered_map<int, int> c;
+        for (int a : A) c[a]++;
+        vector<int> keys;
+        for (auto it : c)
+            keys.push_back(it.first);
+        sort(keys.begin(), keys.end(), [](int i, int j) {return abs(i) < abs(j);});
+        vector<int> res;
+        for (int x : keys) {
+            if (c[x] > c[2 * x]) return {};
+            for (int i = 0; i < c[x]; ++i, c[2 * x]--)
+                res.push_back(x);
+        }
+        return res;
     }
 };
