166171891babb1358e219b323a134ff3aef0f84a
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-09-16 18:13:30 +0530

    Time: 132 ms (5.40%), Space: 15.9 MB (33.67%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -8,7 +8,7 @@ public:
             {
                 count+=nums[i-1]-nums[i]+1;
                 cout<<count<<" ";
-                nums[i]+=nums[i-1]-nums[i]+1;
+                nums[i]=nums[i-1]+1;
             }
         }
         return count;
