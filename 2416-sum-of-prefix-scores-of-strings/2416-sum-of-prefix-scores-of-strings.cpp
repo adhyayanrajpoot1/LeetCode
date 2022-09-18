417cc8d94400c72efdc902cfdcbb93976346c118
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-09-18 23:16:08 +0530

    Time: 595 ms (62.50%), Space: 448.1 MB (37.50%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
@@ -3,100 +3,29 @@ public:
     
     // structure for TrieNode
     
-    struct TrieNode
-    {
-        TrieNode* child[26];
-        
-        int count;
-        
-        TrieNode()
-        {
-            count = 0;
-            
-            for(int i = 0; i < 26; i++)
-            {
-                child[i] = NULL;
+        struct Trie {
+        Trie* ch[26] = {};
+        int cnt = 0;
+        void insert(string &w, int i = 0) {
+            auto n = this;
+            for (auto c : w) {
+                if (n->ch[c - 'a'] == nullptr)
+                    n->ch[c - 'a'] = new Trie();
+                n = n->ch[c - 'a'];
+                ++n->cnt;
             }
         }
-    };
-    
-    // declare a global root
-    
-    TrieNode* root = new TrieNode();
-    
-    // function for insert
-    
-    void insert(string str)
-    {
-        int n = str.size();
-        
-        TrieNode* curr = root;
-        
-        for(int i = 0; i < n; i++)
-        {
-            int idx = str[i] - 'a';
-            
-            // insert a new node
-            
-            if(curr -> child[idx] == NULL)
-            {
-                curr -> child[idx] = new TrieNode(); 
-            }
-            
-            // increment the count of prefix
-            
-            curr -> child[idx] -> count++;
-            
-            // move curr pointer
-          
-            curr = curr -> child[idx];
+        int count(string &w, int i = 0) {
+            return cnt + (i == w.size() ? 0 : ch[w[i] - 'a']->count(w, i + 1));
         }
-    }
-    
-    // function for get_sum
-    
-    int get_sum(string str)
-    {
-        int n = str.size();
-        
-        int sum = 0;
-        
-        TrieNode* curr = root;
-        
-        for(int i = 0; i < n; i++)
-        {
-            int idx = str[i] - 'a';
-            
-            sum += curr -> child[idx] -> count;
-            
-            curr = curr -> child[idx];
-        }
-        
-        return sum;
-    }
-    
+    }; 
     vector<int> sumPrefixScores(vector<string>& words) {
-        
-        int n = words.size();
-        
-        // insert all the words into trie
-        
-        for(int i = 0; i < n; i++)
-        {
-            insert(words[i]);
-        }
-        
-        vector<int> res(n, 0);
-        
-        // find sum for every word
-        
-        for(int i = 0; i < n; i++)
-        {
-            int sum = get_sum(words[i]);
-            
-            res[i] = sum;
-        }
-        
+        Trie t;
+        for (auto &w : words)
+            t.insert(w);
+        vector<int> res;
+        for (auto &w : words)
+            res.push_back(t.count(w));
         return res;
     }
 };
