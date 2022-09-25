e433006815957133da78613f24b204567615f2c2
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-09-25 19:05:56 +0530

    Time: 854 ms (67.48%), Space: 150.6 MB (13.96%) - LeetHub

---
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -0,0 +1,19 @@
+#define pii pair<int, int>
+class Solution {
+public:
+    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
+        vector<vector<pii>> rideStartAt(n);
+        for (auto& ride : rides) {
+            int s = ride[0], e = ride[1], t = ride[2];
+            rideStartAt[s].push_back({e, e - s + t}); 
+        }
+        vector<long long> dp(n+1,0);
+        for (int i = n-1; i >= 1; --i) {
+            for (auto& [e, d] : rideStartAt[i]) {
+                dp[i] = max(dp[i], dp[e] + d);
+            }
+            dp[i] = max(dp[i], dp[i + 1]);
+        }
+        return dp[1];
+    }
+};
