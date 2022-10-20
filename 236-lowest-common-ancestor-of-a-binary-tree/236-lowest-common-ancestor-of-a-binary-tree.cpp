7e28a0e5d0f19bb0e1b3e5e73198c62f66a854ea
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-10-20 15:40:15 +0530

    Time: 35 ms (40.06%), Space: 14.1 MB (97.40%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,51 +9,11 @@
  */
 class Solution {
 public:
-    
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    if (!root || root == p || root == q) return root;
-    TreeNode* left = lowestCommonAncestor(root->left, p, q);
-    TreeNode* right = lowestCommonAncestor(root->right, p, q);
-    return left==NULL ? right : right==NULL ? left : root;
-}
-//      bool f(vector<TreeNode*>&ans , TreeNode*root , TreeNode*temp)
-//     {
-//         if(root==NULL)return false;
-//         ans.push_back(root);
-//         if(root==temp){
-//             //cout<<"fsjv";
-//             //ans.push_back(temp);
-//             return true;
-//         }
-        
-//         if((f(ans,root->left,temp))||(f(ans,root->right , temp))) return true;
-        
-        
-//         ans.pop_back();
-//         return false;
-//     }
-//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-//         vector<TreeNode*>arr1,arr2;
-//         f(arr1 , root , p);
-//         f(arr2 , root , q);
-//         for(int i = 0 ; i < arr1.size() ; i++)cout << arr1[i]->val << " ";
-//         cout<<endl;
-//         for(int i = 0 ; i < arr2.size() ; i++)cout << arr2[i]->val << " ";
-//         cout<<endl;
-//         int i = 0 , j = 0;
-//         TreeNode*ans=NULL;
-//         while(i < arr1.size() && j < arr2.size()){
-//             if(arr1[i]==arr2[j]){
-//                 cout << arr1[i]->val<<" ";
-//                 ans = arr1[i];
-                
-                
-//             }
-//             i++,j++;
-//             //else break;
-//         }
-//         return ans;
-        
-//     }
+        if(root==NULL || root==p || root==q)return root;
+        //if(root->left == p || root->right==q)return root;
+        TreeNode*node1 = lowestCommonAncestor(root->left,p,q);
+        TreeNode*node2= lowestCommonAncestor(root->right,p,q);
+        return node1==NULL ? node2:node2==NULL ? node1 : root;
+    }
 };
