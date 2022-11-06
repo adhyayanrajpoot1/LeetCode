fe5ed53c79a631aa2d69f62759ac7bf7db0e4bb8
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-06 21:40:57 +0530

    Time: 7 ms (63.22%), Space: 10.8 MB (52.30%) - LeetHub

--- 899-orderly-queue/899-orderly-queue.cpp
+++ 899-orderly-queue/899-orderly-queue.cpp
@@ -5,9 +5,9 @@ public:
             sort(S.begin(), S.end());
             return S;
         }
-        string res = S;
+         string res = S;
         for (int i = 1; i < S.length(); i++)
             res = min(res, S.substr(i) + S.substr(0, i));
-        return res;
+         return res;
     }
 };
