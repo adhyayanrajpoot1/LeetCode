d245e1850d211d9ea58f83dff6c3782786089d8e
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-10 18:02:26 +0530

    Time: 10 ms (68.28%), Space: 7.3 MB (90.81%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        string str="";
-        for(int i = 0 ; i < s.size() ; i++)
-        {
-            if(s[i]>='A' && s[i]<='Z')
-            {
-                str+=(s[i]-'A'+'a');
-            }
-            else if(s[i]>='a' && s[i]<='z')
-            {
-                str+=s[i];
-            }
-            else if(s[i]>='0' && s[i]<='9')
-            {
-                str+=s[i];
-            }
-        }
-        int i = 0 , j = str.size()-1;
-        while(i<j)
-        {
-            if(str[i++]==str[j--])continue;
-            return false;
-        }
-        return true;
-    }
+   bool isPalindrome(string s) {
+	int start=0, end=s.length()-1;
+	while(start<=end) {
+		if (!isalnum(s[start])) start++;
+		else if (!isalnum(s[end])) end--;
+		else {
+			if (tolower(s[start++])!=tolower(s[end--])) return false;
+		}
+	}
+	return true;
+   }
 };
