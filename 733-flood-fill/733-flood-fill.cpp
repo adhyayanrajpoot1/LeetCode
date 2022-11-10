093884e79543fbcb398d0002c8422981571aff3d
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-10 10:45:36 +0530

    Time: 24 ms (24.34%), Space: 13.8 MB (90.88%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    int original;
-    void flood_fill(int r , int c , vector<vector<int>>& image,int color)
+    void dfs(int i , int j , int color , int target , vector<vector<int>>&image)
     {
         int n = image.size();
         int m = image[0].size();
-        image[r][c] = color;
-        int d[] = {0,1,0,-1,0};
-        
-        for(int k = 0;  k < 4 ; k++)
+        if(i<0 || j<0 || i==n || j==m || image[i][j]==color)return;
+        if(image[i][j]==target)
         {
-            int dx = r+d[k];
-            int dy = c+d[k+1];
-            if(dx>=0 && dy>=0 && dx<n && dy<m && image[dx][dy]==original)
-            {
-                flood_fill(dx,dy,image,color);
-            }
+            image[i][j] = color;
+            
+            dfs(i+1,j,color,target,image);
+            dfs(i,j+1,color,target,image);
+            dfs(i-1,j,color,target,image);
+            dfs(i,j-1,color,target,image);
+            //image[i][j] = target
         }
-        
     }
-    vector<vector<int>>vis;
+    
+    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        vis.resize(n,vector<int>(m,0));
-        this->original = image[sr][sc];
-        if(original==color)return image;
-        flood_fill(sr,sc,image,color);
+        int target = image[sr][sc];
+        
+        dfs(sr,sc,color,target,image);
         return image;
+        
     }
 };
