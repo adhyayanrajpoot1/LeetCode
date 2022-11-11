4feea4190f74da42307f7401509a099b04bc9647
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-11 15:26:58 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (78.49%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    
-    void f(vector<int>& nums ,vector<vector<int>>&ans , vector<int>&smallAns,vector<int>&freq)
+    void f(int ind , vector<int>&nums , vector<vector<int>>&ans)
     {
-        //base case
-        if(smallAns.size()==freq.size())
-        {
-            ans.push_back(smallAns);
+        if(ind==nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i = 0 ; i < freq.size();i++)
+        for(int i = ind ; i < nums.size() ; i++)
         {
-            if(!freq[i])
-            {
-                freq[i]=1;
-                smallAns.push_back(nums[i]);
-                f(nums,ans,smallAns,freq);
-                smallAns.pop_back();
-                freq[i]=0;
-            }
+            swap(nums[i],nums[ind]);
+            
+            f(ind+1,nums,ans);
+            swap(nums[i],nums[ind]);
+            //f(ind+1,nums,ans);
         }
         
     }
-    
-    
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
-        int size = 1<<n;
         vector<vector<int>>ans;
-        vector<int>freq(n,0);
-        vector<int>smallAns;
-        f(nums,ans,smallAns,freq);
+        
+        f(0,nums,ans);
         return ans;
     }
 };
