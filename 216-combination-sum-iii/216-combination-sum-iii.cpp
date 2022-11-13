f3aa975919f1c1103f9d3b890c63451e537bc692
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-13 16:21:00 +0530

    Time: 4 ms (41.17%), Space: 6.8 MB (22.18%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void f(int ind , int sum , int tar , int k ,vector<int>&ds,vector<int>&nums)
+    void f( int sum , int tar , int k ,vector<int>&ds,int num)
     {
         if(tar<sum)return;
         if(tar==sum)
@@ -10,21 +10,21 @@ public:
                 ans.push_back(ds);
             return;
         }
-        if(ind==nums.size())return;
-        
-        ds.push_back(nums[ind]);
-        sum+=nums[ind];
-        f(ind+1 , sum,tar,k-1,ds,nums);
+        //if(ind==nums.size())return;
+        if(num>9)return;
+        ds.push_back(num);
+        sum+=num;
+        f( sum,tar,k-1,ds,num+1);
         ds.pop_back();
-        sum-=nums[ind];
-        f(ind+1,sum,tar,k,ds,nums);
+        sum-=num;
+        f(sum,tar,k,ds,num+1);
             
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>nums(9);
-        iota(nums.begin() , nums.end(),1);
+        //vector<int>nums(9);
+        //iota(nums.begin() , nums.end(),1);
         vector<int>temp;
-        f(0,0,n,k,temp,nums);
+        f(0,n,k,temp,1);
         return ans;
     }
 };
