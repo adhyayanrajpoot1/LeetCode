23cc1c8835382fbd0c1258ac4dde5783653d3d52
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-13 14:35:14 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (33.33%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-   int dp[2001] = {};
-bool pal[2001][2001] = {};
-int dfs(int i, int k, string &s) {
-    if (i >= s.size())
-        return 0;
-    if (!dp[i]) {
-        dp[i] = 1 + dfs(i + 1, k, s);
-        for (int j = i + k - 1; j < s.size(); ++j)
-            if (pal[i][j])
-                dp[i] = max(dp[i], 2 + dfs(j + 1, k, s));
-        
+   int maxPalindromes(string s, int k) {
+    int n = s.size(), last = INT_MIN, ans = 0;
+    vector<vector<int>> intervals;
+    for (int center = 0; center < 2 * n; center++) {
+        int left = center / 2;
+        int right = left + center % 2;
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right + 1 - left >= k) {
+                intervals.push_back({left, right + 1});
+                break;
+            }
+            left--; right++;
+        }
     }
-    return dp[i] - 1;
-}
-int maxPalindromes(string s, int k) {
-    int n = s.size();
-    for (int i = 0; i < n; ++i)
-        pal[i][i] = true;
-    for (int i = 0; i < n - 1; ++i)
-        pal[i][i + 1] = (s[i] == s[i + 1]);
-    for (int len = 3; len <= n; ++len)
-        for (int i = 0, j = i + len - 1; j < n; ++i, ++j)
-            pal[i][j] = pal[i + 1][j - 1] && (s[i] == s[j]);   
-    return k == 1 ? s.size() : dfs(0, k, s);
+    for (vector<int>& v: intervals) {
+        if (v[0] >= last) {
+            last = v[1];
+            ans++;
+        }
+        else
+            if (v[1] < last)
+                last = v[1];
+    }
+    return ans;
 }
 };
