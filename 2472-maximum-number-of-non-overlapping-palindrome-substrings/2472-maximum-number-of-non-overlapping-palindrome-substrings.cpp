0bd5f3e9ae9cab833398fa4ae31a694dcf919813
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-13 14:56:23 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (33.33%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
    int maxPalindromes(string s, int k) {
-    int n = s.size(), ans = 0, start = 0;
+    int n = s.size(), last = INT_MIN, ans = 0;
+    vector<vector<int>> intervals;
     for (int center = 0; center < 2 * n; center++) {
         int left = center / 2;
         int right = left + center % 2;
-        while (left >= start && right < n && s[left] == s[right]) {
+        while (left >= 0 && right < n && s[left] == s[right]) {
             if (right + 1 - left >= k) {
-                ans++;
-                start = right + 1;
+                intervals.push_back({left, right + 1});
                 break;
             }
             left--; right++;
         }
+    }
+    for (vector<int>& v: intervals) {
+        if (v[0] >= last) {
+            last = v[1];
+            ans++;
+        }
+        
     }
     return ans;
 }
