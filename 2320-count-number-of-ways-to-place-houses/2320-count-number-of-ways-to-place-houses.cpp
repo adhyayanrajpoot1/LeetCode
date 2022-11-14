9f8ff3c337253ad5100009e7bc1efd7099c18629
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-14 16:14:05 +0530

    Time: 203 ms (6.83%), Space: 58.6 MB (6.18%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -1,57 +1,32 @@
 #define  ll long long;
 class Solution {
 public:
-    
     long long find(int curr,int n,vector<vector<int>>&dp){
-
      if(n==1){
-
          return 1;
-
-         
-
      }
-
      if(dp[n][curr]!=-1) return dp[n][curr];
-
      int one=0;
-
      int zero=find(0,n-1,dp);
-
      if(curr!=1){
-
          one=find(1,n-1,dp);
-
      }
-
-     
-
      return dp[n][curr]=(one + zero)%mod;
-
  }
-
-//  ll countStrings(int n) {
-//     if(n==0)return 0;
-//      vector<vector<int>>dp(n+1,vector<int>(2,-1));
-
-//      return (find(0,n,dp)+find(1,n,dp))%mod;
-
-//  }
     
     int mod = 1e9+7;
-    int  f(int ind,int pick , int n,vector<vector<int>>&dp)
-    {
-        
+    int  f(int pick , int ind,vector<vector<int>>&dp)
+    { 
         if(ind==1)
         {   
             return 1;
         }
-        if(dp[ind][pick]==-1)return dp[ind][pick];
+        if(dp[ind][pick]!=-1)return dp[ind][pick];
         int place = 0;
-        int notPlace = f(ind-1,1,n,dp);
-        if(pick==1)
+        int notPlace = f(0,ind-1,dp);
+        if(pick!=1)
         {
-            place = f(ind-1 , 0,n,dp);
+            place = f( 1,ind-1,dp);
         }
         return dp[ind][pick] = (place+notPlace)%mod;
     }
@@ -59,10 +34,10 @@ public:
     int countHousePlacements(int n) {
         vector<vector<int>>dp(n+1,vector<int>(2,-1));
 
-     long long t =  (find(0,n,dp)+find(1,n,dp))%mod;
+     long long t =  (f(0,n,dp) +f(1,n,dp))%mod;
         return (t*t)%mod;
         //vector<vector<int>>dp(n+2,vector<int>(2,-1));
-        int total =  f(n,0,n,dp) +f(n,1,n,dp);
-        return total*total;
+        //int total =  f(n,0,n,dp) +f(n,1,n,dp);
+        //return total*total;
     }
 };
