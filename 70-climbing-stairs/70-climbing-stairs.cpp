a9175233d2a16c733d5060b600f3281c8aeb0f42
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-14 14:34:10 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.39%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -10,7 +10,19 @@ public:
         
     }
     int climbStairs(int n) {
-        vector<int>dp(n,-1);
+        vector<int>dp(n+1,0);
+        dp[n]=1;
+        for(int i = n-1 ; i>=0 ; i--)
+        {
+            if(i+2<=n)
+            {
+                dp[i] = dp[i+2];
+            }
+            dp[i]+=dp[i+1];
+        }
+        return dp[0];
+        
+        
         return f(0,n,dp);
     }
 };
