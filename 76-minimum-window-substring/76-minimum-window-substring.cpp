e9ecfa1c3ffdb898093b29345cbcf41164c12789
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-15 14:25:46 +0530

    Time: 46 ms (36.88%), Space: 8 MB (30.99%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,86 +1,52 @@
 class Solution {
 public:
-    string minWindow(string s , string t) {
-        int n = s.length();
-        int m = t.length();
-        if(n<m)return "";
-        unordered_map<char,int>mp;
-        for(auto it : t) mp[it]++;
-        int i = 0 , j = 0, mn = n+1 , count= mp.size();
-        string res = s;
-        while(j<n) {
-            if(mp.find(s[j])!=mp.end()) {
-                mp[s[j]]--;
-                if(mp[s[j]]==0)count--;
+    string minWindow(string s, string t) {
+        //if(s.size()<t.size())return "";
+        int count= 0;
+        unordered_map<char,int>m;
+        for(auto it : t){
+            if(m.find(it)==m.end())
+            {
+                count++;
             }
-            if(count==0) {
-                while(count==0 && i<=j) {
-                    if(mp.find(s[i])!=mp.end()) {
-                        mp[s[i]]++;
-                        if(mp[s[i]]==1){
-                            if(mn > j-i+1){
-                                mn = j-i+1;
-                                res = s.substr(i,mn);
-                            }
-                            count++;
+            m[it]++;
+        }
+        
+        int i = 0;
+        int j = 0;
+        int start = 0;
+        int ans = INT_MAX;
+        while(j<s.size())
+        {
+        
+            m[s[j]]--;
+            
+            if(m[s[j]]==0)
+            {
+                count--;
+                if(count==0)
+                {
+                    while(count==0)
+                    {
+                        if(j-i+1<ans)
+                        {
+                            ans = j-i+1;
+                            start = i;
                         }
+                        m[s[i]]++;
+                        if(m[s[i]]==1)count++;
+                        i++;
                     }
-                    i++;
                 }
+                
             }
             j++;
+            
         }
-        if(mn==n+1)return "";
-        return res;
+        if(ans==INT_MAX)return "";
+        return s.substr(start,ans);
+        
+        
+        
     }
-//     string minWindow(string s, string t) {
-//         if(s.length()<t.length())
-//             return "";
-        
-//         unordered_map<char,int>umap;
-//         int i=0,j=0,mn=s.size()+1,count=0;
-//         string res="";
-        
-//         for(int i=0;i<t.length();i++)
-//             umap[t[i]]++;
-        
-//         count=umap.size();
-//         //SW
-//         while(j<s.length()){
-//             //cals
-//             if(umap.find(s[j])!=umap.end()){
-//                 umap[s[j]]--;
-//                 //decrease count if val is 0
-//                 if(umap[s[j]]==0)
-//                     count--;
-//             }
-
-//             //window hit
-//             if(count==0){
-//                 // if(mn>j-i+1){
-//                 //     mn=j-i+1;
-//                 //     res=s.substr(i,mn);
-//                 //     }
-                
-//                 while(i<=j && count==0){
-//                     if(umap.find(s[i])!=umap.end()){
-//                         umap[s[i]]++;
-                        
-//                         if(umap[s[i]]==1){
-//                             if(mn>j-i+1){
-//                                 mn=j-i+1;
-//                                 res=s.substr(i,mn);
-//                             }
-//                             count++;
-//                         }
-//                     }
-//                     i++;
-//                 }
-//             }
-//             j++;
-//         }
-//         if(mn==INT_MAX)
-//             return "";
-//         return res;
-//     }
 };
