8f3adae9bb8f0ad242f9cbb9fb1a8e8ff10e7a13
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-15 14:01:27 +0530

    Time: 76 ms (20.48%), Space: 30.9 MB (63.27%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -11,29 +11,49 @@
  */
 class Solution {
 public:
+   
+
     int countNodes(TreeNode* root) {
-        if(root==NULL)return 0;
-        int count= 0;
-        queue<TreeNode*>q;
-        q.push(root);
-        
-        while(!q.empty())
-        {
-            auto node = q.front();
-            q.pop();
-            count++;
-            if(node->left==NULL){
-                return q.size()+count;
-            }
-            else
-            {
-                q.push(node->left);
-            }
-            if(node->right==NULL){
-                return q.size()+count;
-            }
-            else q.push(node->right);
-        }
-        return count;
+
+        if(!root) return 0;
+
+        int hl=0, hr=0;
+
+        TreeNode *l=root, *r=root;
+
+        while(l) {hl++;l=l->left;}
+
+        while(r) {hr++;r=r->right;}
+
+        if(hl==hr) return pow(2,hl)-1;
+
+        return 1+countNodes(root->left)+countNodes(root->right);
+
     }
+
+//     int countNodes(TreeNode* root) {
+//         if(root==NULL)return 0;
+//         int count= 0;
+//         queue<TreeNode*>q;
+//         q.push(root);
+        
+//         while(!q.empty())
+//         {
+//             auto node = q.front();
+//             q.pop();
+//             count++;
+//             if(node->left==NULL){
+//                 return q.size()+count;
+//             }
+//             else
+//             {
+//                 q.push(node->left);
+//             }
+//             if(node->right==NULL){
+//                 return q.size()+count;
+//             }
+//             else q.push(node->right);
+//         }
+//         return count;
+//     }
 };
