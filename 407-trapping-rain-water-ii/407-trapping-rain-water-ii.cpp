3e95f91c5ee8b79e42a7b5ea535c2b22e70cf550
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-16 15:44:17 +0530

    Time: 82 ms (93.62%), Space: 13.7 MB (68.74%) - LeetHub

--- 407-trapping-rain-water-ii/407-trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/407-trapping-rain-water-ii.cpp
@@ -1,100 +1,48 @@
 class Solution {
 public:
-    
-int trapRainWater(vector<vector<int>> &height)
-{
-
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> mn;
-    int n = height.size();
-    int m = height[0].size();
-    //bool vis[m][n];
-    //memset(vis, false, sizeof(vis));
-       vector<vector<int>>vis(n,vector<int>(m,0));
-            for(int i = 0 ; i < n ; i++)
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        int n = heightMap.size();
+        int m = heightMap[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>pq;
+        for(int i = 0 ; i < n ; i++)
         {
             vis[i][0] = vis[i][m-1] = 1;
-            mn.push({height[i][0] , {i,0}});
-            mn.push({height[i][m-1] , {i,m-1}});
+            pq.push({heightMap[i][0] , {i,0}});
+            pq.push({heightMap[i][m-1] , {i,m-1}});
         }
         for(int i = 0 ; i < m ; i++)
         {
             vis[0][i] = vis[n-1][i] = 1;
-            mn.push({height[0][i] , {0,i}});
-            mn.push({height[n-1][i] , {n-1,i}});
+            pq.push({heightMap[0][i] , {0,i}});
+            pq.push({heightMap[n-1][i] , {n-1,i}});
         }
-    int dr[] = {-1,0,1,0,-1};
-    int water = 0;
-
-    while (mn.size() > 0)
-    {
-        pair<int, pair<int, int>> p = mn.top();
-        mn.pop();
-        for (int i = 0; i < 4; i++)
+        int dr[] = {-1,0,1,0,-1};
+        int ans= 0;
+        while(!pq.empty())
         {
-            int rowdash = p.second.first + dr[i];
-            int coldash = p.second.second + dr[i+1];
-
-            if (rowdash >= 0 && rowdash < n && coldash >= 0 && coldash < m && vis[rowdash][coldash] == false)
+            auto node = pq.top();
+            pq.pop();
+            int h = node.first;
+            int i = node.second.first;
+            int j = node.second.second;
+            
+            for(int k = 0 ;k < 4 ; k++)
             {
-                water += max(0, p.first - height[rowdash][coldash]);
-
-                if (p.first > height[rowdash][coldash])
-                    mn.push({p.first,{rowdash,coldash}});
-                else
-                    mn.push({height[rowdash][coldash],{rowdash, coldash}});
-                vis[rowdash][coldash] = true;
+                int x = i+dr[k];
+                int y = j+dr[k+1];
+                if(x>=0 && y>=0 && x<n && y<m && vis[x][y]==0)
+                {
+                    ans+=max(0,h-heightMap[x][y]);
+                    pq.push({max(h,heightMap[x][y]) , {x,y}});
+                    vis[x][y]=1;
+                    
+                }
             }
             
         }
+        return ans;
+        
+        
     }
-    return water;
-}
 };
-// class Solution {
-// public:
-//     int trapRainWater(vector<vector<int>>& heightMap) {
-//         int n = heightMap.size();
-//         int m = heightMap[0].size();
-//         vector<vector<int>>vis(n,vector<int>(m,0));
-//         priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int,pair<int,int>>>>pq;
-//         for(int i = 0 ; i < n ; i++)
-//         {
-//             vis[i][0] = vis[i][m-1] = 1;
-//             pq.push({heightMap[i][0] , {i,0}});
-//             pq.push({heightMap[i][m-1] , {i,m-1}});
-//         }
-//         for(int i = 0 ; i < m ; i++)
-//         {
-//             vis[0][i] = vis[n-1][i] = 1;
-//             pq.push({heightMap[0][i] , {0,i}});
-//             pq.push({heightMap[n-1][i] , {n-1,i}});
-//         }
-//         int dr[] = {-1,0,1,0,-1};
-//         int ans= 0;
-//         while(!pq.empty())
-//         {
-//             auto node = pq.top();
-//             pq.pop();
-//             int h = node.first;
-//             int i = node.second.first;
-//             int j = node.second.second;
-            
-//             for(int i = 0 ;i < 4 ; i++)
-//             {
-//                 int x = i+dr[i];
-//                 int y = j+dr[i+1];
-//                 if(x>=0 && y>=0 && x<n && y<m && vis[x][y]==0)
-//                 {
-//                     ans+=max(0,h-heightMap[x][y]);
-//                     pq.push({max(h,heightMap[x][y]) , {x,y}});
-//                     vis[x][y]=1;
-                    
-//                 }
-//             }
-            
-//         }
-//         return ans;
-        
-        
-//     }
-// };
