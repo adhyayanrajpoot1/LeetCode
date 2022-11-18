0e3dc7d9c60422871bcac686db69bbd0afeac685
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-18 17:44:52 +0530

    Time: 122 ms (75.45%), Space: 35.6 MB (16.05%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -21,7 +21,7 @@ public:
             pq.pop();
             long long ele = node.second;
             long long dis = node.first;
-            //if(dis>dist[ele])
+            if(dis>dist[ele])continue;
             
             for(auto it : adj[ele])
             {
@@ -49,26 +49,3 @@ public:
         return ways[n-1];
     }
 };
-//     int dijkstra(const vector<vector<pll>>& graph, int n, int src) {
-//         vector<ll> dist(n, LONG_MAX);
-//         vector<ll> ways(n);
-//         ways[src] = 1;
-//         dist[src] = 0;
-//         priority_queue<pll, vector<pll>, greater<>> minHeap;
-//         minHeap.push({0, 0}); // dist, src
-//         while (!minHeap.empty()) {
-//             auto[d, u] = minHeap.top(); minHeap.pop();
-//             if (d > dist[u]) continue; // Skip if `d` is not updated to latest version!
-//             for(auto [v, time] : graph[u]) {
-//                 if (dist[v] > d + time) {
-//                     dist[v] = d + time;
-//                     ways[v] = ways[u];
-//                     minHeap.push({dist[v], v});
-//                 } else if (dist[v] == d + time) {
-//                     ways[v] = (ways[v] + ways[u]) % MOD;
-//                 }
-//             }
-//         }
-//         return ways[n-1];
-//     }
-// };
