d6fca97e3c9c353ef677ba80ae4e700610b5f52a
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-18 16:43:54 +0530

    Time: 479 ms (60.65%), Space: 90.1 MB (95.52%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
-    
-    int maxLength = -1;
-    
-    void getcycle(vector<int> &edges,int si,vector<bool>& visit,vector<int>& store){
-        if(si == -1)return ;
-        if(visit[si]){
-            int count = -1;
-            for(int i =0;i<store.size();i++){
-                if(store[i]==si){
-                    count = i;
-                    break;
-                } }
-            
-            if(count==-1)return;
-            int size = (store.size()-count);
-            maxLength = max(maxLength,size);
-            return ;
-        }
-        
-        visit[si] = true;
-        store.push_back(si);
-        getcycle(edges,edges[si],visit,store);
-        
-        
-        return ;
-        
-    }
-    
-    
-    
     int longestCycle(vector<int>& edges) {
-        
-        vector<bool> visit(edges.size(),0);
-        
-        for(int i =0;i<edges.size();i++){
-            
-            if(visit[i])continue;
-            vector<int> store;
-            getcycle(edges,i,visit,store);
-     
+        int ans= -1;
+        vector<int>idx(edges.size(),0);
+        for(int i = 0 , k =1; i <edges.size() ; i++)
+        {
+            int temp = k;
+            int j = i;
+            while(j!=-1 && !idx[j])
+            {
+                idx[j] = k++;
+                j = edges[j];
+            }
+            if(j!=-1 && idx[j] >=temp)
+            {
+                ans=max(ans , k-idx[j]);
+            }
         }
-        
-        return maxLength;
-        
+        return ans;
     }
 };
+// int longestCycle(vector<int>& edges) {
+//     int n = edges.size(), ans = -1;
+//     vector<int> idx(n);
+//     for (int i = 0, k = 1; i < n; i++) {
+//         int j = i, curk = k;
+//         while (j != -1 && !idx[j]) {
+//             idx[j] = k++;
+//             j = edges[j];
+//         }
+//         if (j != -1 && idx[j] >= curk)
+//             ans = max(ans, k - idx[j]);
+//     }
+//     return ans;
+// }
