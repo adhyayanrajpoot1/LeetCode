3a0c52fa22480382080d4ba4a188513028292ea3
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-18 15:52:44 +0530

    Time: 1486 ms (57.50%), Space: 197.7 MB (62.50%) - LeetHub

--- 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
@@ -1,41 +1,55 @@
 class Solution {
 public:
     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
-        vector<vector<int>> adj(n + 1);
-        // Build the adjacency list.
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        vector<int>adj[n+1];
+        for(auto it : edges)
+        {
+            int i = it[0];
+            int j = it[1];
+            adj[i].push_back(j);
+            adj[j].push_back(i);
         }
-
-        queue<pair<int, int>> q;
-        vector<int> dist1(n + 1, -1), dist2(n + 1, -1);
-        // Start with node 1, with its minimum distance.
-        q.push({1, 1});
-        dist1[1] = 0;
-
-        while (!q.empty()) {
-            auto [node, freq] = q.front();
+        vector<int>mini(n+1,-1) ; vector<int>smini(n+1,-1);
+        queue<pair<int,int>>q;
+        q.push({1,1});
+        mini[1] = 0;
+        
+        while(!q.empty())
+        {
+            auto node = q.front();
             q.pop();
-
-            int timeTaken = freq == 1 ? dist1[node] : dist2[node];
-            // If the timeTaken falls under the red bracket, wait till the path turns green.
-            if ((timeTaken / change) % 2) {
-                timeTaken = change * (timeTaken / change + 1) + time;
-            } else {
-                timeTaken = timeTaken + time;
+            int ele = node.first;
+            //freq[ele]++;
+            //if(fre[ele]==2)
+            int freq = node.second;
+            int signal = freq==1 ? mini[ele] : smini[ele];
+            if((signal/change)%2)
+            {
+                signal = change*(signal/change+1)+time;
             }
-
-            for (auto& neighbor : adj[node]) {
-                if (dist1[neighbor] == -1) {
-                    dist1[neighbor] = timeTaken;
-                    q.push({neighbor, 1});
-                } else if (dist2[neighbor] == -1 && dist1[neighbor] != timeTaken) {
-                    if (neighbor == n) return timeTaken;
-                    dist2[neighbor] = timeTaken;
-                    q.push({neighbor, 2});
+            else
+            {
+                signal = signal+time;
+            }
+            for(auto it : adj[ele])
+            {
+                if(mini[it]==-1)
+                {
+                    mini[it] = signal;
+                    q.push({it,1});
+                }
+                else
+                {
+                    if(smini[it]==-1 && mini[it]!=signal)
+                    {
+                        q.push({it,2});
+                        smini[it] = signal;
+                        if(it==n)return signal;
+                    }
                 }
             }
+            
+            
         }
         return 0;
     }
