d73341096094dfeff514630b73147909acb60ab0
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-11-18 18:10:01 +0530

    Time: 23 ms (95.80%), Space: 13.3 MB (21.04%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    vector<int> findBall(vector<vector<int>>& grid) {
-        vector<int> result(grid[0].size(), 0);
-        for (int i = 0; i < grid[0].size(); i++) {
-            result[i] = findBallDropColumn(0, i, grid);
-        }
-        return result;
+    int dfs(int row , int col , vector<vector<int>>&grid)
+    {
+        if(row==grid.size())return col;
+        int newCol = col +grid[row][col];
+        if(newCol<0 || newCol >grid[0].size()-1 || grid[row][col]!=grid[row][newCol])return -1;
+        return dfs(row+1,newCol,grid);
     }
-
-    int findBallDropColumn(int row, int col, vector<vector<int>>& grid) {
-        // base case; ball reached the last row
-        if (row == grid.size()) return col;
-        int nextColumn = col + grid[row][col];
-        if (nextColumn < 0 || nextColumn > grid[0].size() - 1 ||
-            grid[row][col] != grid[row][nextColumn]) {
-            return -1;
+    vector<int> findBall(vector<vector<int>>& grid) {
+        vector<int>ans(grid[0].size());
+        for(int i = 0 ; i < grid[0].size() ; i++)
+        {
+            ans[i] = dfs(0,i , grid);
         }
-        return findBallDropColumn(row + 1, nextColumn, grid);
+        return ans;
     }
 };
