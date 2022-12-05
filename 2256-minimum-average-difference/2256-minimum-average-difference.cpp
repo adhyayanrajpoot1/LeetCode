970912b837380934e825e16e4833d5f9c9c01518
Adhyayan Rajpoot <64541015+adhyayanrajpoot1@users.noreply.github.com>
2022-12-05 17:47:35 +0530

    Time: 179 ms (70.28%), Space: 78.3 MB (76.57%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
-        int n = int(nums.size());
-        int ans = -1;
-        int minAvgDiff = numeric_limits<int>::max();
-        
-        // Generate prefix and suffix sum arrays.
-        vector<long long> prefixSum(n + 1);
-        vector<long long> suffixSum(n + 1);
-        
-        for (int index = 0; index < n; ++index) {
-            prefixSum[index + 1] = prefixSum[index] + nums[index];
+        int n = nums.size();
+        //vector<long long>pre(n,0);
+        //pre[0] = nums[0];
+        long long sum = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            sum+=nums[i];
+            //pre[i] = pre[i-1]+nums[i];
         }
-        
-        for (int index = n - 1; index >= 0; --index) {
-            suffixSum[index] = suffixSum[index + 1] + nums[index];
-        }
-        
-        for (int index = 0; index < n; ++index) {
-            // Calculate average of left part of array, index 0 to i.
-            long long leftPartAverage = prefixSum[index + 1];
-            leftPartAverage /= (index + 1);
-            
-            // Calculate average of right part of array, index i+1 to n-1.
-            long long rightPartAverage = suffixSum[index + 1];
-            // If right part have 0 elements, then we don't divide by 0.
-            if (index != n - 1) {
-                rightPartAverage /= (n - index - 1);
-            }
-            
-            int currDifference = int(abs(leftPartAverage - rightPartAverage));
-            // If current difference of averages is smaller,
-            // then current index can be our answer.
-            if (currDifference < minAvgDiff) {
-                minAvgDiff = currDifference;
-                ans = index;
+        long long ans = INT_MAX;
+        int index = -1;
+        long long sumI = 0;
+        long long lsum = 0;
+        long long rsum = sum;
+        for(int i = 0 ; i < n ; i++)
+        {
+            sumI += nums[i];
+            lsum +=nums[i];
+            rsum -=nums[i];
+            int a = (lsum/(i+1));
+            int b = (i==(n-1)) ? 0 : rsum/((n-i-1));
+            int small = abs( a-b );
+            if(small < ans)
+            {
+                index = i;
+                ans = small;
             }
         }
-
-        return ans;
+        return index;
     }
 };
